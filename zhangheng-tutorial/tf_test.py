8042889b60800106cfe9b4bb1be3ffc3ae35b6ef
zhangheng <zhanghengsjtu@gmail.com>
2018-05-08 19:54:32 +0800

    nothing changed

--- zhangheng-tutorial/tf_test.py
+++ zhangheng-tutorial/tf_test.py
@@ -2,7 +2,45 @@
 import tensorflow as tf
 import numpy as np
 
-# 使用 NumPy 生成假数据(phony data), 总共 100 个点.
+
+
+# 创建一个变量, 初始化为标量 0.
+state = tf.Variable(0, name="counter")
+
+# 创建一个 op, 其作用是使 state 增加 1
+
+one = tf.constant(1)
+new_value = tf.add(state, one)
+update = tf.assign(state, new_value)
+
+# 这一句话也需要run，因为上面的的语句只是定义，并没有去执行
+init_op = tf.initialize_all_variables()
+
+# 启动图, 运行 op
+with tf.Session() as sess:
+  # 运行 'init' op
+  sess.run(init_op)
+  # 打印 'state' 的初始值
+  print sess.run(state)
+  # 运行 op, 更新 'state', 并打印 'state'
+  for _ in range(3):
+    sess.run(update)
+    print sess.run(state)
+
+""" matrix1 = tf.constant([[3., 3.]])
+matrix2 = tf.constant([[2.],[2.]])
+product = tf.matmul(matrix1, matrix2)
+
+print matrix1
+print matrix2
+print product
+
+sess = tf.Session()
+result = sess.run(product)
+print result
+sess.close() """
+
+""" # 使用 NumPy 生成假数据(phony data), 总共 100 个点.
 x_data = np.float32(np.random.rand(2, 100)) # 随机输入
 y_data = np.dot([0.100, 0.200], x_data) + 0.300
 
@@ -30,5 +68,5 @@ for step in xrange(0, 201):
     if step % 20 == 0:
         print step, sess.run(W), sess.run(b)
 
-# 得到最佳拟合结果 W: [[0.100  0.200]], b: [0.300]
+# 得到最佳拟合结果 W: [[0.100  0.200]], b: [0.300] """
 
